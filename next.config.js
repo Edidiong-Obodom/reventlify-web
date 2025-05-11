479da9f843eb1c6d5a63dce364ac662cca117769
Edidiong Obodom <edijay17@gmail.com>
2025-05-11 15:56:07 +0100

    refactor: remove redundant next config option :hammer:

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "i.pravatar.cc", "illustrations.popsy.co"],
   },
-  experimental: {
-    appDir: true, // ✅ THIS IS CRITICAL FOR NEXTAUTH TO KNOW YOU'RE USING APP ROUTER
-  },
 };
 
 module.exports = nextConfig;
