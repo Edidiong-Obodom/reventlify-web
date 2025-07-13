f5d9d67c6aef9944c20f3f4d794848eb7bf179f0
Edidiong Obodom <edijay17@gmail.com>
2025-07-13 16:19:25 +0100

    build ish fix

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -4,8 +4,14 @@ export const metadata = {
     "Sign in to your world of endless events. Discover, join, and experience unforgettable moments tailored just for you.",
 };
 
+import FullScreenLoader from "@/components/common/loaders/fullScreenLoader";
 import LoginPageWrapper from "@/components/signin/wrapper";
+import { Suspense } from "react";
 
 export default function LoginPage() {
-  return <LoginPageWrapper />;
+  return (
+    <Suspense fallback={<FullScreenLoader backGround="light" />}>
+      <LoginPageWrapper />
+    </Suspense>
+  );
 }
